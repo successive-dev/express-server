9c0ecff9a5f6f2b3123efcea6bdbaca29c641341
successive-dev <vishal.tewatia@successive.tech>
2019-03-07 12:11:56 +0530

    Removed the dist folder

--- dist/extraTs/utils/permissions.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const constants_1 = require("../constants");
-function hasPermission(moduleName, role, permissionType) {
-    return constants_1.permissions[moduleName][permissionType].includes(role);
-}
-exports.default = hasPermission;
-//# sourceMappingURL=permissions.js.map
