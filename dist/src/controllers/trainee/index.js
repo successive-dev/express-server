d305102577135345ddf7697dacf50afb33918a42
successive-dev <vishal.tewatia@successive.tech>
2019-03-07 13:27:01 +0530

    fixed untracked files

--- dist/src/controllers/trainee/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const Controller_1 = require("./Controller");
-exports.trainee = Controller_1.default;
-const routes_1 = require("./routes");
-exports.traineeRouter = routes_1.default;
-//# sourceMappingURL=index.js.map
--- dist/src/repositories/user/UserModel.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const mongoose = require("mongoose");
-const UserSchema_1 = require("./UserSchema");
-// interface IUserModel extends mongoose.Document {}
-exports.User = mongoose.model("User", UserSchema_1.UserSchema);
-//# sourceMappingURL=UserModel.js.map
