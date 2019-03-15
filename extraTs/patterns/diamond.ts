a0e200f3515876c91b14b4f6676c6b39b3e1a56e
successive-dev <vishal.tewatia@successive.tech>
2019-03-15 17:21:46 +0530

    fix: lint errors

--- extraTs/patterns/diamond.ts
+++ extraTs/patterns/diamond.ts
@@ -1,37 +1,37 @@
 function print_diamond(n: number) {
-    const numRows = n;
-    const halfRows = Math.ceil(numRows / 2);
+  const numRows = n;
+  const halfRows = Math.ceil(numRows / 2);
 
-    // forward pattern
-    for (let row = 1; row <= halfRows; row++) {
-        let line = '';
+  // forward pattern
+  for (let row = 1; row <= halfRows; row++) {
+    let line = '';
 
-        // appending spaces
-        for (let space = halfRows; space > row; space--) {
-            line = line + ' ';
-        }
-        // appending stars
-        for (let stars = 1; stars <= 2 * row - 1; stars++) {
-            line = line + '*';
-        }
-        // print line
-        console.log(line);
+    // appending spaces
+    for (let space = halfRows; space > row; space--) {
+      line = line + ' ';
     }
-
-    // rev pattern
-
-    for (let row = halfRows - 1; row >= 1; row--) {
-        let line = '';
-
-        // appending spaces to line
-        for (let space = halfRows; space > row; space--) {
-            line = line + ' ';
-        }
-        for (let stars = 2 * row - 1; stars >= 1; stars--) {
-            line = line + '*';
-        }
-        console.log(line);
+    // appending stars
+    for (let stars = 1; stars <= 2 * row - 1; stars++) {
+      line = line + '*';
     }
+    // print line
+    console.log(line);
+  }
+
+  // rev pattern
+
+  for (let row = halfRows - 1; row >= 1; row--) {
+    let line = '';
+
+    // appending spaces to line
+    for (let space = halfRows; space > row; space--) {
+      line = line + ' ';
+    }
+    for (let stars = 2 * row - 1; stars >= 1; stars--) {
+      line = line + '*';
+    }
+    console.log(line);
+  }
 }
 
 // print_diamond(10);
--- extraTs/patterns/triangle.ts
+++ extraTs/patterns/triangle.ts
@@ -1,22 +1,22 @@
 function print_triangle(n: number) {
-    // getting the number of rows to be printed
-    const numRows = n;
+  // getting the number of rows to be printed
+  const numRows = n;
 
-    // forward pattern
-    for (let row = 1; row <= numRows; row++) {
-        let line = '';
+  // forward pattern
+  for (let row = 1; row <= numRows; row++) {
+    let line = '';
 
-        // appending spaces
-        for (let space = numRows; space > row; space--) {
-            line = line + ' ';
-        }
-        // appending stars
-        for (let stars = 1; stars <= 2 * row - 1; stars++) {
-            line = line + '*';
-        }
-        // print line
-        console.log(line);
+    // appending spaces
+    for (let space = numRows; space > row; space--) {
+      line = line + ' ';
     }
+    // appending stars
+    for (let stars = 1; stars <= 2 * row - 1; stars++) {
+      line = line + '*';
+    }
+    // print line
+    console.log(line);
+  }
 }
 
 export default print_triangle;
